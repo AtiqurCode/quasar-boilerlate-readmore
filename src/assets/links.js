6f8778f7155249c1700e62c2097cf87d4765b7e5
S.M. Shakil <sm.shakil@itconquest.com>
2022-07-28 19:43:22 +0600

    Add left navigation links

---
+++ src/assets/links.js
@@ -0,0 +1,47 @@
+export const navLinks = [
+  {
+    label: 'Dashboard',
+    icon: 'home',
+    hash: '/dashboard'
+  },
+  {
+    label: 'Companies',
+    icon: 'card_travel',
+    hash: '/companies'
+  },
+  {
+    label: 'Projects',
+    icon: 'format_list_numbered',
+    hash: '/projects'
+  },
+  // {
+  //   label: 'Projects & Jobs',
+  //   icon: 'format_list_numbered',
+  //   hash: '/projects-jobs'
+  // },
+  {
+    label: 'Customers',
+    icon: 'supervised_user_circle',
+    hash: '/customers'
+  },
+  {
+    label: 'Contacts',
+    icon: 'contacts',
+    hash: '/contacts'
+  },
+  {
+    label: 'User Access',
+    icon: 'verified_user',
+    hash: '/user-access'
+  },
+  // {
+  //   label: 'Inbox',
+  //   icon: 'far fa-envelope',
+  //   hash: '/inbox'
+  // },
+  {
+    label: 'Settings',
+    icon: 'settings',
+    hash: '/settings'
+  }
+]
