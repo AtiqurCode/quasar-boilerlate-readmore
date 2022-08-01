c53cc870e18d510d19cdf2274373eaa196661ac2
S.M. Shakil <100042874+forhadusa@users.noreply.github.com>
2022-08-01 13:53:41 +0600

    Update project configurations

--- quasar.config.js
+++ quasar.config.js
@@ -30,7 +30,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'client'
+      'notify-defaults'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -110,7 +110,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
