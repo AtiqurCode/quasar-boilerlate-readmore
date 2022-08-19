da20d26d1c899ad586b2cfdaa555fc345ebb4e53
S.M. Shakil <sm.shakil@itconquest.com>
2022-08-19 18:00:52 +0600

    Convert reset-password component in vue 3 composition api

--- src/modules/auth/pages/ResetPassword.vue
+++ src/modules/auth/pages/ResetPassword.vue
@@ -1,99 +1,119 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from 'stores/auth-store'
+import { useCommonStore } from 'stores/common-store'
+import CompanyBranding from 'components/CompanyBranding.vue'
+
+const router = useRouter()
+
+const form = reactive({
+  password: '',
+  re_type_password: '',
+  verification_code: ''
+})
+const isPwd = ref(true)
+
+const commonStore = useCommonStore()
+const authStore = useAuthStore()
+const onSubmit = async () => {
+  try {
+    await authStore.forgotPassword(form)
+    router.push({ name: 'login' })
+  } catch (error) {}
+}
+</script>
+
 <template>
-  <q-page padding>
+  <q-page
+    padding
+    class="row justify-center items-center"
+  >
     <q-card
-      style="max-width: 400px;"
-      class="q-mx-auto q-mt-lg"
-      square
+      style="width: 400px; max-width: 100%;"
+      class="text-center"
       flat
     >
+      <q-card-section>
+        <CompanyBranding class="q-mb-xl" />
+      </q-card-section>
       <q-form
         @submit="onSubmit"
         class="q-gutter-md"
       >
-        <q-card-section class="q-pt-lg">
-          <q-img
-            src="~assets/logo_full.png"
-            spinner-color="primary"
-            contain
-            style="max-height: 50px; max-width: 100%;"
-          />
-        </q-card-section>
         <q-card-section>
           <q-input
-            v-model.trim="password"
+            v-model.trim="form.password"
+            class="itc-input required"
+            stack-label
+            outlined
             label="New Password"
             placeholder="Type your new Password"
             :type="isPwd ? 'password' : 'text'"
             :rules="[
-              val => !!val || 'Field is required',
-              val => val.length >= 6 || 'Minimum length 6'
+              val => !!val || 'Password is required',
+              val => val.length >= 6 || 'Minimum 6 characters required'
             ]"
           >
-            <template v-slot:append>
+            <template #append>
               <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
+                :name="isPwd ? 'o_visibility' : 'o_visibility_off'"
                 class="cursor-pointer"
                 @click="isPwd = !isPwd"
               />
             </template>
           </q-input>
           <q-input
-            v-model.trim="re_type_password"
+            v-model="form.re_type_password"
+            class="itc-input required"
+            stack-label
+            outlined
             label="Confirm Password"
             placeholder="Confirm Password"
             type="password"
-            :rules="[val => val === password || 'Password doesn\'t match']"
+            :rules="[
+              val => val === form.password || 'Password doesn\'t match'
+            ]"
+          />
+          <q-input
+            v-model="form.verification_code"
+            class="itc-input required"
+            stack-label
+            outlined
+            label="Verification Code"
+            placeholder="XXXX"
+            type="text"
+            :rules="[
+              val => !!val || 'Verification Code is required'
+            ]"
           />
         </q-card-section>
 
-        <q-card-section class="q-pt-none">
+        <q-card-actions class="q-py-none">
+          <q-btn
+            label="Update Password"
+            color="primary"
+            size="lg"
+            no-caps
+            class="full-width"
+            :loading="commonStore.loading"
+            type="submit"
+          />
+        </q-card-actions>
+        <q-card-section class="q-pt-none text-caption">
+          If you are here by mistake, please go to
           <q-btn
             flat
             dense
+            no-caps
+            size="0.75rem"
+            class="q-py-none"
             color="primary"
-            label="UPDATE PASSWORD"
-            type="submit"
+            label="Login"
+            @click="router.push({name: 'login'})"
           />
         </q-card-section>
       </q-form>
     </q-card>
   </q-page>
 </template>
-
-<script>
-import { validateEmail } from 'src/validationServices'
-import { mapActions } from 'vuex'
-
-export default {
-  data () {
-    return {
-      password: '',
-      re_type_password: '',
-      verification_code: '',
-      isPwd: true
-    }
-  },
-  created () {
-    this.verification_code = this.$route.query.verification_code
-  },
-  methods: {
-    ...mapActions({
-      forgotPassword: 'auth/forgotPassword'
-    }),
-    validateEmail,
-    async onSubmit () {
-      try {
-        const data = {
-          password: this.password,
-          re_type_password: this.re_type_password,
-          verification_code: this.verification_code
-        }
-        await this.forgotPassword(data)
-        this.$router.push({ name: 'confirmation', params: { message: 'You have successfully changed your password. Feel free to login with your new password.' } })
-      } catch (error) {}
-    }
-  }
-}
-</script>
-
-<style></style>
--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,10 +1,10 @@
 export default [
   {
-    path: '/login',
+    path: '/auth',
     component: () => import('layouts/AuthLayout.vue'),
     children: [
       {
-        path: '',
+        path: 'login',
         component: () => import('./pages/LoginIndex.vue'),
         name: 'login',
         meta: {
@@ -20,33 +20,26 @@ export default [
           requiresAuth: false,
           title: 'Recover Password'
         }
+      },
+      {
+        path: 'confirmation',
+        component: () => import('./pages/VerificationCodeConfirmation.vue'),
+        name: 'confirmation',
+        props: true,
+        meta: {
+          requiresAuth: false,
+          title: 'confirmation'
+        }
+      },
+      {
+        path: 'reset-password',
+        component: () => import('./pages/ResetPassword.vue'),
+        name: 'reset-password',
+        meta: {
+          requiresAuth: false,
+          title: 'reset password'
+        }
       }
-      //     {
-      //       path: 'confirmation',
-      //       component: () => import('./pages/EmailConfirmation.vue'),
-      //       name: 'confirmation',
-      //       props: true,
-      //       meta: {
-      //         requiresAuth: false,
-      //         title: 'confirmation'
-      //       }
-      //     }
     ]
-  // },
-  // {
-  //   path: '/reset-password',
-  //   component: () => import('layouts/AuthLayout'),
-  //   children: [
-  //     {
-  //       path: '',
-  //       component: () => import('./pages/ResetPassword.vue'),
-  //       name: 'reset-password',
-  //       meta: {
-  //         requiresAuth: false,
-  //         title: 'reset password'
-  //       }
-  //     }
-  //   ]
   }
-
 ]
